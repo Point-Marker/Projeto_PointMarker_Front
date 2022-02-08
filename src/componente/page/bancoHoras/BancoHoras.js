1a1226c305152f96873a3c3c1be490202e299465
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-02-08 13:28:13 -0300

    Criar pagina de Banco de Horas.

---
+++ src/componente/page/bancoHoras/BancoHoras.js
@@ -0,0 +1,79 @@
+import react, { Component } from "react";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import Header from "../../header/Header";
+
+export default class BancoDeHoras extends Component {
+  constructor(props) {
+    
+    super(props);
+    this.state = {
+      message: this.props.state ? this.props.state.message : "",
+    };
+  }
+
+  save = () => {
+    const url = "http://localhost:8080/leads";
+    let data = {
+      email: this.email,
+      nome: this.name,
+      descricao: this.observation,
+    };
+    const requestInfo = {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+    };
+    fetch(url, requestInfo)
+      .then((response) => {
+        console.log(this.email, this.name, this.observation);
+        return response;
+      })
+      .catch((e) => {
+        this.setState({ message: e.message });
+        console.log(this.email, this.nome, this.observacoes);
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <Header title="Banco de Horas" />
+        <hr />
+        <Form>
+          <FormGroup>
+            <Label for="name"> Nome do Funcionario: </Label>
+            <Input
+              type="text"
+              id="name"
+              onChange={(e) => (this.name = e.target.value)}
+              placeholder="Informe o seu nome"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="horario"> Entrada: </Label>
+            <Input
+              type="time"
+              id="entrada"
+              onChange={(e) => (this.email = e.target.value)}
+              placeholder="Informe o seu horário de entrada"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="horario"> Saida: </Label>
+            <Input
+              type="time"
+              id="saida"
+              onChange={(e) => (this.observation = e.target.value)}
+              placeholder="Informe o seu horário de saida"
+            />
+          </FormGroup>
+          <Button color="danger" block onClick={this.save}>
+            ENVIAR
+          </Button>
+        </Form>
+      </div>
+    );
+  }
+}
