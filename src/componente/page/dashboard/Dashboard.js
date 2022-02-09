0c2c196b34de536bf1c67d367110af5a1274fe4a
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-02-09 08:27:47 -0300

    Criar lista de banco de horas.

--- src/componente/page/dashboard/Dashboard.js
+++ src/componente/page/dashboard/Dashboard.js
@@ -1,12 +1,48 @@
+import { Tab } from "bootstrap";
 import react, { Component } from "react";
+import {
+  Table,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Alert,
+} from "reactstrap";
+
+class ListBancoDeHoras extends Component {
+  render() {
+    return (
+     <Table className="table-bordered text-center">
+         <thead className="thead-dark">
+             <tr>
+                 <th>Funcionario</th>
+                 <th>Dia do Trabalho</th>
+                 <th>Entrada</th>
+                 <th>Saida</th>
+                 <th>Actions</th>
+             </tr>
+
+         </thead>
+     </Table>
+    );
+  }
+}
 
 class Dashboard extends Component {
   render() {
     return (
       <div>
-        <h1>DASHBOARD</h1>
-        <h2>Você foi Logado Com Sucesso</h2>
-      </div>
+        <div className="row">
+          <div className="col-md-6 my-3">
+           
+            </div>
+            <div className="col-md-6 my-3">
+            <h2 className="font-weight-bold text-center ">LISTA DE BANCO DE HORAS</h2>
+                <ListBancoDeHoras/>
+            </div>
+          </div>
+        </div>
     );
   }
 }
