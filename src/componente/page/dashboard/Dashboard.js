87159bfc62dd43786886eb0efb6acc5dabe29a0b
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-02-08 22:16:41 -0300

    Criar tela de dashboard.

---
+++ src/componente/page/dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import react, { Component } from "react";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h1>DASHBOARD</h1>
+        <h2>Você foi Logado Com Sucesso</h2>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
