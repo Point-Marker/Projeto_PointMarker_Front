9b9d562a6bbef9159355694c256bc09d2ec03a25
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-02-11 18:51:34 -0300

    Incluir navbar.

--- src/componente/admin/page/dashboard-principal/TelaPrincipal.js
+++ src/componente/admin/page/dashboard-principal/TelaPrincipal.js
@@ -7,6 +7,7 @@ import {
   Label,
   Input,
   Alert,
+  Navbar,
 } from "reactstrap";
 import Header from "../../../header/Header";
 import "../dashboard-principal/estilo.css";
@@ -15,10 +16,7 @@ class ListaFuncionarios extends Component {
   render() {
     const { funcionarios } = this.props;
     return (
-      <Table
-        className="table-bordered border-dark text-center"
-        id="tabela"
-      >
+      <Table className="table-bordered border-dark text-center" id="tabela">
         <thead className="thead-dark text-dark " id="linhas">
           <tr>
             <th>Funcionario</th>
@@ -80,10 +78,64 @@ class TelaPrincipal extends Component {
   render() {
     return (
       <div>
-        <Header title="Bem Vindo" />
-          <h2 className="font-weight-bold text-center "></h2>
-          <ListaFuncionarios funcionarios={this.state.funcionarios} />
+        <div>
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="/dashboard">
+                Point Marker
+              </a>
+              <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a
+                      class="nav-link active"
+                      aria-current="page"
+                      href="/dashboard"
+                    >
+                      Home
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/atualizacaohoras">
+                      Atualizar Banco de Horas
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/cadastro">
+                      Cadastrar Funcionario
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/bancohoras">
+                      Bater o Ponto
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/">
+                      Sair
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
         </div>
+        <hr />
+        <Header title="Bem Vindo" />
+        <h2 className="font-weight-bold text-center "></h2>
+        <ListaFuncionarios funcionarios={this.state.funcionarios} />
+      </div>
     );
   }
 }
